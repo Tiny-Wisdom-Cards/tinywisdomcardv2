f43ac5c145db9394e9b6de6acf889e1765d173a9
IPP-World <indrapaneru100@gmail.com>
2026-07-30 17:28:28 +0545

    Add Google tag to root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -125,7 +125,15 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en" className={fontVars}>
-      <head />
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K1RB7B496K"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-K1RB7B496K');
+        ` }} />
+      </head>
       <body>
         {children}
         <Analytics />
